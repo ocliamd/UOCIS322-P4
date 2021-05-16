cdfc1d713a0c88835e4dbfa0a6741c3155200859
LIam Dauphinee <ocliam@me.com>
2021-05-15 22:04:57 -0700

    second commit

--- brevets/acp_times.py
+++ brevets/acp_times.py
@@ -26,7 +26,6 @@ def open_time(control_dist_km, brevet_dist_km, brevet_start_time):
        A date object indicating the control open time.
        This will be in the same time zone as the brevet start time.
     """
-    
     return arrow.now()
 
 
