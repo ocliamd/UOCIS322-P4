27f23a4ec3921e4d647e6ae8231be010000ddbc8
LIam Dauphinee <ocliam@me.com>
2021-05-16 23:24:52 -0700

    fixed bugs in tests part 1, 600km test still failing

--- brevets/tests/test_600.py
+++ brevets/tests/test_600.py
@@ -1,6 +1,6 @@
 import nose
 import arrow
-from brevets import acp_times
+import acp_times
 
 def test_open_600():
     assert str(acp_times.open_time(399, 600, arrow.get('2020-01-01T00:00:00'))) == "2020-01-01T12:06:00+00:00"
